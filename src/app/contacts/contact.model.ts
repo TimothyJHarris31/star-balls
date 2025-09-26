35a1b4afd7d89f537aaa100f73fd41582b175d47
starb520 <97459015+starb520@users.noreply.github.com>
2025-09-26 17:03:54 -0600

    worked through the second class assignment for wdd 430

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,17 @@
+export class Contact {
+    public id: string;
+    public name: string;
+    public email: string;
+    public phone: string;
+    public imageUrl: string;
+    public group: Contact[] | null;
+
+    constructor(id: string, name: string, email: string, phone: string, imageUrl: string, grp: Contact[] | null=null) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+        this.group = grp;
+    }
+}
