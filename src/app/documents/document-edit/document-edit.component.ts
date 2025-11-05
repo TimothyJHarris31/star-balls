f6e7c22ba080b39c737f9a4b09f4df18dcf45139
starb520 <97459015+starb520@users.noreply.github.com>
2025-11-05 15:14:14 -0700

    got the documents edit working using an angular template driven form

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Document } from '../document.model';
 import { NgForm } from '@angular/forms';
+import { DocumentService } from '../document.service';
 @Component({
   selector: 'cms-document-edit',
   standalone: false,
@@ -13,13 +15,48 @@ export class DocumentEditComponent implements OnInit {
   document: Document;
   editMode: boolean = false;
 
-  constructor() {}
+  constructor(private documentService: DocumentService,
+              private router: Router,
+              private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(){}
+  ngOnInit(){
+    this.route.params.subscribe(params => {
+      const id = params['id'];  
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocument(id);
+      if (!this.originalDocument) {
+        return;
+      } 
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
+  }
 
-  onSubmit(form: NgForm) {}
-  
-  onCancel() {}
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newDocument = new Document(
+      '',
+      value.name,
+      value.description,
+      value.url,
+      []
+    );
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.router.navigate(['/documents']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
+    this.editMode = false;
+  }
 
 
 }
